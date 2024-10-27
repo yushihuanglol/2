25e469d3dc21769554f728548e400692710c3bca
1 <147516506+yushihuanglol@users.noreply.github.com>
2024-10-27 23:32:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function inputWord() {
+    const word = prompt("Введите слово(норма слов):");
+    const monitorImage = document.getElementById("monitorImage");
+
+    if (word === '06.09.2024') {
+        monitorImage.src = 'new_image.png'; // Замените на Ваше изображение
+        setTimeout(() => {
+            monitorImage.src = 'new_image1.png'; // Возврат к исходному изображению через 3 секунды
+        }, 3000);
+    } else {
+        monitorImage.src = 'not_correct.png'; // Картинка с фразой "не то"
+        setTimeout(() => {
+            monitorImage.src = 'placeholder.png'; // Возврат к исходному изображению через 3 секунды
+        }, 3000);
+    }
+}
